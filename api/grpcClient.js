9d97e8a3a1429549fc2f00a684c68c1e13e1b264
kzeta11 <112991280+kzeta11@users.noreply.github.com>
2025-06-15 14:41:11 -0400

    feat: Add invoice, monitoring, social, and video services with CRUD operations
    - Implemented invoiceService with Create, Read, Update, Delete, and List functionalities.
    - Added monitoringService to list actions and errors.
    - Created socialService for liking and commenting on videos, and retrieving interactions.
    - Developed videoService for managing videos with Create, Read, Update, Delete, and List operations.
    - Updated package.json to include dotenv and express dependencies.

--- api/grpcClient.js
+++ api/grpcClient.js
@@ -1,26 +1,58 @@
 const { credentials } = require("@grpc/grpc-js");
-const loadProto = require("./src/utils/loadproto")
+const loadProto = require("./src/utils/loadproto");
 const { config } = require("dotenv");
 
+config();
+
 const loadClients = (app) => {
-    const authProto = loadProto("auth");
-    const playlistProto = loadProto("playlist");
-    const userProto = loadProto("users");
+  // Auth Service
+  const authProto = loadProto("auth");
+  app.locals.authClient = new authProto.AuthService(
+    process.env.AUTH_SERVICE_URL,
+    credentials.createInsecure()
+  );
 
-    app.locals.authClient = new authProto.AuthService(
-        process.env.AUTH_SERVICE_URL,
-        credentials.createInsecure()
-    );
+  // Playlist Service
+  const playlistProto = loadProto("playlist");
+  app.locals.playlistsClient = new playlistProto.PlaylistService(
+    process.env.PLAYLIST_SERVICE_URL,
+    credentials.createInsecure()
+  );
 
-    app.locals.playlistsClient = new playlistProto.PlaylistService(
-        process.env.PLAYLIST_SERVICE_URL,
-        credentials.createInsecure()
-    );
+  // Users Service
+  const userProto = loadProto("users");
+  app.locals.usersClient = new userProto.Users(
+    process.env.USER_SERVICE_URL,
+    credentials.createInsecure()
+  );
 
-    app.locals.usersClient = new userProto.Users(
-        process.env.USER_SERVICE_URL,
-        credentials.createInsecure()
-    );
-}
+  // Video Service
+  const videoProto = loadProto("video");
+  app.locals.videoClient = new videoProto.VideoService(
+    process.env.VIDEO_SERVICE_URL,
+    credentials.createInsecure()
+  );
 
-module.exports = loadProto;
+  // Invoice Service
+  const invoiceProto = loadProto("invoice");
+  app.locals.invoiceClient = new invoiceProto.InvoiceService(
+    process.env.INVOICE_SERVICE_URL,
+    credentials.createInsecure()
+  );
+
+  // Monitoring Service
+  const monitoringProto = loadProto("monitoring");
+  app.locals.monitoringClient = new monitoringProto.MonitoringService(
+    process.env.MONITORING_SERVICE_URL,
+    credentials.createInsecure()
+  );
+
+  // Social Service
+  const socialProto = loadProto("social");
+  app.locals.socialClient = new socialProto.SocialService(
+    process.env.SOCIAL_SERVICE_URL,
+    credentials.createInsecure()
+  );
+};
+
+module.exports = loadClients;
